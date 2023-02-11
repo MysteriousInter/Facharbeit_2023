ff2aa2240da0d62ddd03017456e760fbab93c8cf
Florian Korittky <florian.korittky@gmail.com>
2023-02-11 20:00:22 +0100

    initial

--- src/main/java/de/korittky/spielplangenerator/SpielplangeneratorApplication.java
+++ src/main/java/de/korittky/spielplangenerator/SpielplangeneratorApplication.java
@@ -15,35 +15,14 @@ public class SpielplangeneratorApplication {
 	public static void main(String[] args) throws FileNotFoundException {
 		SpringApplication.run(SpielplangeneratorApplication.class, args);
 		//Verein test= new Verein("tollerverein",2);
+		String eingabedatei= "F:/IdeaProjects/Facharbeit/spielplangenerator/src/main/resources/testSpielfest3.txt";
 
-/*
-		Gson gson2 = new Gson();
-		JsonReader reader = new JsonReader(new FileReader("F:/IdeaProjects/Facharbeit/spielplangenerator/src/main/resources/testSpielfest.txt"));
-		Spielfest spielfest = gson2.fromJson(reader, Spielfest.class);
-		spielfest.init();
-		Output test=new Output();
-		test.printTeams(spielfest);
-		FillPlan fillPlan=new FillPlan(spielfest);
-
-		PfdOut xdd =new PfdOut("F://IdeaProjects//Facharbeit//spielplangenerator/test.pdf");
-		Plan plan = new Plan(1, 1);
-		Runde runde1 = new Runde(1);
-		for(Verein verein : spielfest.getVereine()) {
-			for(Team team : verein.getTeams()) {
-				Spiel spiel1 = new Spiel(team, team);
-				runde1.setSpiel(spiel1,0);
-			}
+		if (args.length>0) {
+			eingabedatei=args[0];
 		}
-		plan.setRunde(0,runde1 );
 
-		try {
-			xdd.printPlanPdf(plan);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-*/
 		Gson gson2 = new Gson();
-		JsonReader reader = new JsonReader(new FileReader("F:/IdeaProjects/Facharbeit/spielplangenerator/src/main/resources/testSpielfest3.txt"));
+		JsonReader reader = new JsonReader(new FileReader(eingabedatei));
 		Spielfest spielfest = gson2.fromJson(reader, Spielfest.class);
 		spielfest.init();
 		Plan plan=new Plan(spielfest.getAlleTeams().length,spielfest.getAnzahlRunden());
